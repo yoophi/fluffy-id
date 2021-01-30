106f4349c828928a96892b57589c0e5aebf91daa
Pyunghyuk Yoo <yoophi@gmail.com>
2021-01-30 20:09:15 +0900

    fix repo url in setup.py

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/yoophi/fluffy_id',
+    url='https://github.com/yoophi/fluffy-id',
     version='0.1.0',
     zip_safe=False,
 )
